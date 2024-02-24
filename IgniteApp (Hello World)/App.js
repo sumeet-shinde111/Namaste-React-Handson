5eccb2e63c27a4e3609bb151713eb261a6f2e1b3
sumeetshinde <sumeetshinde111@gmail.com>
2024-02-25 01:12:05 +0530

    seperate javascript code from html file

---
+++ IgniteApp (Hello World)/App.js
@@ -0,0 +1,37 @@
+const heading = React.createElement("h1",{},"This is hello from React!!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+/* 
+In above program
+
+CDN 1: it is core of react which has methods like createElement,useRef,useEffect etc.
+print React in devtools to see the methods..
+
+CND 2: It provides things that are require to manupalate dom like createRoot,render etc
+ print ReactDOM in devtools to see the methods..
+
+ ----------------------------------------------------------------------------------------
+
+ Code Explanation:
+
+IMP methods: 
+    createElement()
+    createRoot()
+    render()
+
+1) To create a h1 tag and render it on browser we need to first create a h1 and text inside it which is done by using createElement
+from js but it uses React.createElement which comes from cdn1.
+React.createElement takes 3 arguments: 
+    1) tag
+    2) object
+    3) text that is to be rendered in tag
+
+2) Now to append created element on body but inside a div which has id root use ReactDom.createRoot(get div id root using js)
+3) We have created an h1 tag and root as well now we have to append that and render it to do so use render method from
+   reactDOM.
+   i.e root.render(heading);
+
+*/
